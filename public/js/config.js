f61070b7e472bf3ae93e117604392e867d0a57c4
raulcodes <rcamacho96@gmail.com>
2017-02-11 18:54:29 -0600

    init commit

---
+++ public/js/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA-x9QZH1YezDMX-GPxsNPyK3H0WwQ5cFw",
+    authDomain: "blistering-heat-648.firebaseapp.com",
+    databaseURL: "https://blistering-heat-648.firebaseio.com",
+    storageBucket: "blistering-heat-648.appspot.com",
+    messagingSenderId: "610839407937"
+  };
+  firebase.initializeApp(config);
